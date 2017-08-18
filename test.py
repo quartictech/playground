b3ddaf47eab8e05627a0da7672c4e229b49175e9
Alex Sparrow <alex@quartic.io>
2017-08-18 17:58:08 +0100

    stderr

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+import sys
 from quartic import step, writer
 
 print("Generating...")
+print("Some error...", file=sys.stderr)
 @step
 def step1(test: "input") -> "output":
     return writer("Something", "Something").json({})
