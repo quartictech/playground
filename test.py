ffe44f334c3dd48a069af6e8231622ba859855b2
Alex Sparrow <alex@quartic.io>
2017-08-18 17:59:04 +0100

    Try exception

--- test.py
+++ test.py
@@ -3,6 +3,8 @@ from quartic import step, writer
 
 print("Generating...")
 print("Some error...", file=sys.stderr)
+
+something_is_wrong
 @step
 def step1(test: "input") -> "output":
     return writer("Something", "Something").json({})
