7f111caf0ee57af0ff8320f9618c085fa35e08dd
Arlo Bryer <arlo@quartic.io>
2017-08-22 18:11:44 +0100

    Simplify test a bit more and add a step

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
-import sys
 from quartic import step, writer
 
-print("Generating...")
-print("Some error...", file=sys.stderr)
 
 @step
 def step1(test: "input") -> "output":
     return writer("Something", "Something").json({})
+
+@step
+def step2(test2: "output") -> "output2":
+    return writer("Something else", "something else").json({})
