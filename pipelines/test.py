915d5b0efeebbca7a9059112a338f24b7f340c72
Alex Sparrow <alex@quartic.io>
2017-08-31 18:27:05 +0100

    Broken thing

--- pipelines/test.py
+++ pipelines/test.py
@@ -1,10 +1,12 @@
 from quartic import step, writer
+from pipelines.noob.test import *
 
+print("YES")
 
 @step
 def step1(test: "input") -> "output":
     return writer("Something", "Something").json({})
 
 @step
-def step2(test2: "output") -> "output2":
+def step2(test2: "output") -> "input":
     return writer("Something else", "something else").json({})
