e1d9a7c81baf5eeafd495d6d8affc45814b82280
Arlo Bryer <arlo@quartic.io>
2017-08-31 15:12:15 +0100

    comment out final step.

--- pipelines/another_test.py
+++ pipelines/another_test.py
@@ -8,3 +8,7 @@ def step1(a_new_test: "new_input") -> "new_output":
 @step
 def step2(test2: "new_output") -> "output3":
     return writer("Something else", "something else").json({})
+
+# @step
+# def step3(in1: "output2", in2: "output3") -> "final_dataset":
+#     return writer("Something else", "something else").json({})
