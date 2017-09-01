91712cef27582d56c5b165f7c5f70887284c1d9d
Arlo Bryer <arlobryer@gmail.com>
2017-09-01 13:55:14 +0000

    bad dag

--- pipelines/another_test.py
+++ pipelines/another_test.py
@@ -10,5 +10,5 @@ def step2(test2: "new_output") -> "output3":
     return writer("Something else", "something else").json({})
 
 @step
-def step3(in0: "output", in1: "output2", in2: "output3", in3: "new_output") -> "final_dataset":
+def step3(in0: "output", in1: "output2", in2: "output3", in3: "new_output") -> "output3":
     return writer("Something else", "something else").json({})
