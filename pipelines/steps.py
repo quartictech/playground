c29c2386ab5c0894ec50224694bc68dbffc3430a
Arlo Bryer <arlo@quartic.io>
2017-09-19 15:28:49 +0100

    fix to account for dfs coming back

--- pipelines/steps.py
+++ pipelines/steps.py
@@ -1,11 +1,11 @@
 from quartic import step, writer
-import pandas as pd
 
 @step
 def join(in1: "employees", in2: "employees_surname", in3: "employees_address") -> "employees_joined":
-    employees = pd.read_csv(in1.reader().csv())
-    emp_surnames = pd.read_csv(in2.reader().csv())
-    emp_addresses = pd.read_csv(in3.reader().csv())
+    #get dataframes and join them
+    employees = in1.reader().csv()
+    emp_surnames = in2.reader().csv()
+    emp_addresses = in3.reader().csv()
 
     merged = employees.merge(emp_surnames, on="Name").merge(emp_addresses, on="Name")
 
