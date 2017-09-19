99d040e1d8bcfb0d9e85b6604e320f796aab6e7e
Arlo Bryer <arlo@quartic.io>
2017-09-19 15:50:11 +0100

    update pipeline

--- pipelines/steps.py
+++ pipelines/steps.py
@@ -1,9 +1,9 @@
 from quartic import step, writer
 
 
-for ds in ['employees', 'employees_surname', 'employees_address']
+for ds in ['employees', 'employees_surname', 'employees_address']:
     @step
-    def clean(in1: ds) -> "clean_{}":
+    def clean(in1: ds) -> "clean_{}".format(ds):
         df = in1.reader().csv()
         df_lower = df.apply(lambda x: x.astype(str).str.lower())
         return writer("Cleaned tables {}".format(ds)).parquet(df_lower)
