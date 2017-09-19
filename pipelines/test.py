7478b1a03d527ca4f1e1b70b643daaff35ffec09
Arlo Bryer <arlo@quartic.io>
2017-09-19 15:16:24 +0100

    build a little pipeline of employee data

--- pipelines/test.py
+++
@@ -1,6 +0,0 @@
-from quartic import step, writer
-from quartic.incubating import raw, FromBucket
-
-@raw
-def register() -> "first_input":
-    return FromBucket("quartic-employees.csv", name="Quartic employees list")
