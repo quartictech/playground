c92aa7e0e9bcf2ec51f185ddc2798f7c89b3a0a3
"Arlo Bryer" <arlobryer@gmail.com>
2017-09-26 19:00:04 +0200

    Quartic - qli commit on 2017-09-26

--- pipelines/clean.py
+++ pipelines/clean.py
@@ -1,5 +1,11 @@
 from quartic import step, writer
 
+@step
+def clean(in1: 'employees') -> "clean_employees":
+    df = in1.reader().csv()
+    df_lower = df.apply(lambda x: x.astype(str).str.lower())
+    return writer("Cleaned table {}".format(ds)).parquet(df_lower)
+
 for ds in ['employees', 'employees_surname', 'employees_address']:
     @step
     def clean(in1: ds) -> "clean_{}".format(ds):
